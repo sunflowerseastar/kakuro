c7dfc80ebc312717975c6ff0b0628c54213a0111
Grant Surlyn <grant@sunflowerseastar.com>
2019-12-10 09:42:56 -0800

    cross sums basic small setup

--- src/cross_sums/core.cljs
+++ src/cross_sums/core.cljs
@@ -4,16 +4,27 @@
    [reagent.core :as reagent :refer [atom create-class]]))
 
 (defn generate-board []
-  [
-   (vec (repeat 3 {}))
-   (vec (repeat 3 {}))
-   (vec (repeat 3 {}))
-   ])
+  [[{:type :black} {:type :summands :right nil :down 4} {:type :summands :right nil :down 6}]
+   [{:type :summands :right 3 :down nil} {:type :entry} {:type :entry}]
+   [{:type :summands :right 7 :down nil} {:type :entry} {:type :entry}]])
 
 (def game (atom {:board (generate-board)}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
+
+(defn square-c [x y square]
+  (let [type (:type square)]
+    [:div.square
+     {:key (str x y)
+      :class type
+      :style {:grid-column (+ x 1) :grid-row (+ y 1)}}
+     (cond (= type :summands)
+           [:div
+            [:span.summand-right (:right square)]
+            [:span.summand-down (:down square)]]
+           (= type :entry)
+           [:span.piece-container ""])]))
 
 (defn main []
   (create-class
@@ -25,15 +36,7 @@
                            (map-indexed
                             (fn [y row]
                               (map-indexed
-                               (fn [x square]
-                                 (let [{:keys [color piece-type]} square]
-                                   [:div.square
-                                    {:key (str x y)
-                                     :class [(if (or (and (even? y) (odd? x)) (and (odd? y) (even? x))) "dark")]
-                                     :style {:grid-column (+ x 1) :grid-row (+ y 1)}}
-                                    (if (not-empty square)
-                                      [:span.piece-container
-                                       {:class [color piece-type]} (:h1 "X")])]))
+                               (fn [x square] [square-c x y square])
                                row))
                             board)]]]))}))
 
