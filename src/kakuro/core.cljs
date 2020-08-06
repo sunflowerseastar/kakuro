ae1a27331393120e5030549d406985fa6df475f9
Grant Surlyn <grant@sunflowerseastar.com>
2020-08-05 22:04:07 -0700

    add "no-solution" state; fixup states

--- src/kakuro/core.cljs
+++ src/kakuro/core.cljs
@@ -15,21 +15,30 @@
 ;;   Each flag is {:direction :down|:right :sum int :distance int}"
 (def board (atom boards/b1))
 
+;; "ui"
 (def is-requesting (atom false))
 (def is-timeout (atom false))
+(def is-no-solution (atom false))
 (def is-success (atom false))
 
-(defn reset-board! [new-board]
+(defn clear-ui! []
   (do (reset! is-requesting false)
       (reset! is-timeout false)
-      (reset! is-success false)
-      (reset! board new-board)))
+      (reset! is-no-solution false)
+      (reset! is-success false)))
+
+(defn reset-board! [new-board]
+  (reset! board new-board))
 
 (defn clear-board! []
   (let [clear-values (fn [squares] (->> squares (mapv #(assoc % :value nil))))]
     (reset-board! (->> @board
                        (map clear-values)
                        vec))))
+
+(defn clear! []
+  (do (clear-ui!)
+      (clear-board!)))
 
 (defn square-c [x y {:keys [flags type] :as square} click-fn dbl-click-fn update-sum-fn x-shape y-shape]
   [:div.square
@@ -51,39 +60,42 @@
          [:span.piece-container (:value square)])])
 
 (defn post-request-solution [flags-to-be-solved]
-  (spyx "post-request-solution" flags-to-be-solved)
+  ;; (spyx "post-request-solution" flags-to-be-solved)
   (do (reset! is-requesting true)
       (POST "http://localhost:3001/solve"
             {:headers {"content-type" "application/edn"}
              :body (str "{:flags-to-be-solved " flags-to-be-solved "}")
-             :handler #(do (reset! is-requesting false)
-                           (reset! is-success true)
-                           (reset-board! (util/board-solution->board-with-solutions @board (:solution %))))
+             :handler #(let [solution (:solution %)]
+                         (do (reset! is-requesting false)
+                             (if (empty? solution)
+                               (reset! is-no-solution true)
+                               (do (reset! is-success true)
+                                   (reset-board! (util/board-solution->board-with-solutions @board solution))))))
              :error-handler #(do (reset! is-requesting false)
                                  (reset! is-timeout true)
                                  (.error js/console (str "error: " %)))})))
 
 (defn change-square-type! [x y new-type]
-  (swap! board assoc-in [y x] {:type new-type :x x :y y :value nil}))
+  (do (clear!)
+      (swap! board assoc-in [y x] {:type new-type :x x :y y :value nil})))
 
 (defn new-flag [direction]
   {direction {:sum 0 :distance nil}})
 
 (defn change-square-to-flag! [x y]
-  (swap! board assoc-in [y x]
-         {:type :flag :x x :y y
-          :flags {:down {:sum 0 :distance nil}
-                  :right {:sum 0 :distance nil}}}))
+  (do (clear!)
+      (swap! board assoc-in [y x]
+             {:type :flag :x x :y y
+              :flags {:down {:sum 0 :distance nil}
+                      :right {:sum 0 :distance nil}}})))
 
 (defn on-click-square [x y {:keys [type]}]
-  (spyx x y type)
   (cond (= type :entry)
         (change-square-type! x y :black)
         (= type :black)
         (change-square-type! x y :entry)))
 
 (defn on-double-click-square [x y {:keys [type]}]
-  (spyx x y type)
   (if (= type :flag)
     (change-square-type! x y :entry)
     (change-square-to-flag! x y)))
@@ -91,7 +103,8 @@
 (defn update-sum-fn [x y e]
   (let [new-sum (-> e .-target .-value js/parseInt)
         direction (.getAttribute (-> e .-target) "data-direction")]
-    (swap! board assoc-in [y x :flags (keyword direction) :sum] new-sum)))
+    (do (clear!)
+        (swap! board assoc-in [y x :flags (keyword direction) :sum] new-sum))))
 
 (defn fix-board! [b]
   (let [new-board (-> b
@@ -118,11 +131,13 @@
                   is-period (= (.-keyCode e) 190)
                   height (count @board)
                   width (-> @board first count)]
-              (cond is-c (clear-board!)
+              (cond is-c (clear!)
                     is-f (fix-board! @board)
                     (or is-enter is-s) (request-solution)
-                    (or is-minus is-comma) (when (and (> width 2) (> height 2)) (reset-board! (util/decrease-board-size @board)))
-                    (or is-plus is-period) (when (and (< width 14) (< height 14)) (reset-board! (util/increase-board-size @board))))))]
+                    (or is-minus is-comma) (when (and (> width 2) (> height 2))
+                                             (do (clear!) (reset-board! (util/decrease-board-size @board))))
+                    (or is-plus is-period) (when (and (< width 14) (< height 14))
+                                             (do (clear!) (reset-board! (util/increase-board-size @board)))))))]
     (create-class
      {:component-did-mount (fn [] (.addEventListener js/document "keydown" keyboard-listeners))
       :reagent-render
@@ -142,8 +157,14 @@
             @board)]]
          [:div.button-container
           [:div.button-indicator
-           {:class [(when @is-success "is-success") (when @is-timeout "is-timeout") (when @is-requesting "is-requesting")]}
-           [:button {:on-click #(when (and (false? @is-success) (false? @is-timeout) (false? @is-requesting))
+           {:class [(when @is-success "is-success")
+                    (when @is-timeout "is-timeout")
+                    (when @is-no-solution "is-no-solution")
+                    (when @is-requesting "is-requesting")]}
+           [:button {:on-click #(when (and (false? @is-success)
+                                           (false? @is-timeout)
+                                           (false? @is-no-solution)
+                                           (false? @is-requesting))
                                   (request-solution))}
             "solve"]]]])})))
 
