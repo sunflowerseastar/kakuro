33c1e85d0ba22ed56d4343b46e2e2461cb3eba00
Grant Surlyn <grant@sunflowerseastar.com>
2020-08-12 22:07:08 -0700

    add test

--- test/kakuro/core_test.cljs
+++ test/kakuro/core_test.cljs
@@ -1,3 +1,8 @@
 (ns kakuro.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]))
+  (:require
+   [cljs.test :refer-macros [deftest is testing run-tests]]
+   [kakuro.utilities :refer [clue-square->clue-notation]]))
+
+(deftest test-clue-square->clue-notation
+  (is (= (clue-square->clue-notation {:type :clue, :x 0, :y 1, :clues {:right {:sum 16, :distance 2}}})
+         '([:r 0 1 16 2]))))
